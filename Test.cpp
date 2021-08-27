959633f4b9887b50b15b949014ecbe4da0b44f4e
Khim Nguyễn <khiem06072007@gmail.com>
2021-08-27 19:54:38 +0700

    Update in main for testing

--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <vector>
 #include "cfraction.h"
 using namespace std;
 
 int main()
 {
-	fraction t = 12.2_frac; //Works fine
+	int a;
+	vector <fraction> vector;
+	while (cin >> a)
+	{
+		vector.emplace_back(a);
+	}
+	for (fraction x:vector)
+	{
+		cout << fraction::simplify(x);
+	}
 }
